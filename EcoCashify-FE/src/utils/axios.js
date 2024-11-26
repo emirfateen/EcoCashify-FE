f71dca0ceb8c72e51d6393dca753d206cbf74f17
kamalMakarim <kamal.makarim@gmail.com>
2024-11-26 15:08:14 +0700

    fix maybe

--- EcoCashify-FE/src/utils/axios.js
+++ EcoCashify-FE/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:5000";
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
 console.log("BASE_URL:", BASE_URL);
 const apiClient = axios.create({
     baseURL: BASE_URL,
